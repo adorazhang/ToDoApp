8c24f9d8be7aa644a81acc67cb5c0bccad1b8000
Qiao Zhang <adora91@gmail.com>
2015-12-09 10:37:24 -0500

    Add Everything

---
+++ README.md
@@ -0,0 +1,22 @@
+# To-Do List Mobile Web App in JavaScript (AngularJS) #
+> By Qiao "Adora" Zhang (adora91@gmail.com)
+
+Option Chosen
+----
+Option #2: Create a mobile web app that allows you create, edit, and view short notes. Notes are just small textual items, like to-do lists.
+- Notes are stored using browser local storage
+- The app should be usable on a standard mobile browser
+
+Overview
+----
+This is a simple to-do app in AngularJS and BootStrap.
+There are three files in total: index.html, controller.js, and css.css. You can view index.html in a standard mobile browser to run this application.
+You need to have internet access to run this application, since libraries used are not included locally.
+
+To Run This Project
+----
+Open index.html in any browser.
+
+License
+----
+MIT
---
+++ controller.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('toDoApp', [])
+        .controller('controller', controller);
+
+    controller.$inject = ['$scope'];
+
+    function controller($scope) {
+        var todoList = this;
+
+        todoList.todos = [
+          { text: 'Lorem ipsum dolor sit amet', done: true, editing: false },
+          { text: 'consectetuer adipiscing elit', done: false, editing: false },
+          { text: 'Aenean commodo ligula eget dolor', done: true, editing: false },
+          { text: 'Aenean massa', done: true, editing: false },
+          { text: 'ultricies nec', done: false, editing: false }];
+
+        todoList.addTodo = function () {
+            todoList.todos.push({ text: todoList.todoText, done: false, editing: false });
+            todoList.todoText = '';
+        };
+
+        todoList.deleteTodo = function (index) {
+            todoList.todos.splice(index, 1);
+        };
+
+        todoList.countDone = function () {
+            var count = 0;
+            angular.forEach(todoList.todos, function (todo) {
+                count += todo.done ? 1 : 0;
+            });
+            return count;
+        };
+
+        todoList.doneEditing = function () {
+        };
+
+        todoList.clearAll = function () {
+            var oldTodos = todoList.todos;
+            todoList.todos = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) todoList.todos.push(todo);
+            });
+        }
+    }
+})();
